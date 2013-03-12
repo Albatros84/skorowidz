8073ed340aae836ebc56739f3ac27162b9140622
Janusz Liro <j.liro@pracowniagier.com>
2013-03-12 12:33:17 +0100

    menu projektu w jQuery by Janusz Liro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,44 @@
 //= require ckeditor-jquery
 //= require ckeditor/ckeditor
 //= require_tree .
+ $(document).ready(function(){
+ 	  $("div.metr").hide();
+      $("h3#metr").click(function(){
+      $("div.metr").toggle('fast','swing');
+     });
+    });
+ 
+  $(document).ready(function(){
+  	  $("div.org").hide();
+      $("h3#org").click(function(){
+      $("div.org").toggle('fast','swing');
+     });
+    });
+    
+    $(document).ready(function(){
+      $("div.mer").hide();
+      $("h3#mer").click(function(){
+      $("div.mer").toggle('fast','swing');
+     });
+    });
+        
+	  $(document).ready(function(){
+      $("div.zespol").hide();
+      $("h3#zespol").click(function(){
+      $("div.zespol").toggle('fast','swing');
+     });
+    });
+    
+ $(document).ready(function(){
+      $("div.przedPro").hide();
+      $("h3#przedPro").click(function(){
+      $("div.przedPro").toggle('fast','swing');
+     });
+    });
 
+    $(document).ready(function(){
+      $("div.poSpotkaniu").hide();
+      $("h3#poSpotkaniu").click(function(){
+      $("div.poSpotkaniu").toggle('fast','swing');
+     });
+    });
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i, :on => :create
   validates_inclusion_of :role, :in => ["admin", "trener","biuro", "sprzedaż"]
     
-protected    
+
     #def self.authenticate(email, password)
   def authenticate(email, password)
     user = User.find_by_email(email)
