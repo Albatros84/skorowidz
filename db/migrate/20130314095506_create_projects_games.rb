81320bf5fdfd1f2ca66fb66ff1ee18832717faeb
Maciek Kasprzyk <maciek@kapustka.net>
2013-03-14 14:01:17 +0100

    fix games-projects, validate game name

--- db/migrate/20130314095506_create_projects_games.rb
+++ db/migrate/20130314095506_create_projects_games.rb
@@ -1,14 +1,14 @@
 class CreateProjectsGames < ActiveRecord::Migration
   def up
-    create_table :projects_games, id: false do |t|
+    create_table :games_projects, id: false do |t|
       t.integer :project_id, null: false
       t.integer :game_id, null: false
     end
-    add_index :projects_games, [:project_id, :game_id], unique: true
+    add_index :games_projects, [:project_id, :game_id], unique: true
   end
 
   def down
-    remove_index :projects_games, column: [:project_id, :game_id]
-    drop_table :projects_games
+    remove_index :games_projects, column: [:project_id, :game_id]
+    drop_table :games_projects
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20130314095506) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "games_projects", :id => false, :force => true do |t|
+    t.integer "project_id", :null => false
+    t.integer "game_id",    :null => false
+  end
+
+  add_index "games_projects", ["project_id", "game_id"], :name => "index_games_projects_on_project_id_and_game_id", :unique => true
+
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.string   "client"
@@ -161,13 +168,6 @@ ActiveRecord::Schema.define(:version => 20130314095506) do
   add_index "projects", ["subject_history_id"], :name => "fk_subject_history_id_histories"
   add_index "projects", ["target_agreed_with_customer_history_id"], :name => "fk_target_agreed_with_customer_history_id_histories"
   add_index "projects", ["transport_details_history_id"], :name => "fk_transport_details_history_id_histories"
-
-  create_table "projects_games", :id => false, :force => true do |t|
-    t.integer "project_id", :null => false
-    t.integer "game_id",    :null => false
-  end
-
-  add_index "projects_games", ["project_id", "game_id"], :name => "index_projects_games_on_project_id_and_game_id", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email"
