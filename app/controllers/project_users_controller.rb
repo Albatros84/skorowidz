c0418e3c6d836e5ea5cca4351f46ebdb57306628
Janusz Liro <januszliro@gmail.com>
2013-03-24 19:14:44 +0100

    dodane Ajaxowe guziczki usuwające uczestników by Janusz L

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -73,11 +73,14 @@ class ProjectUsersController < ApplicationController
   # DELETE /project_users/1.json
   def destroy
     @project_user = ProjectUser.find(params[:id])
+    id=@project_user.project_id  
     @project_user.destroy
-
+    @user_project_roles=ProjectUser.find_all_by_project_id(id) 
+     
     respond_to do |format|
-      format.html { redirect_to project_users_url }
-      format.json { head :no_content }
+      format.js
+     format.html { redirect_to projects }
+      #format.json { head :no_content }
     end
   end
 end
